01acdec92355bfeff58d9ca18db261ca12536b9b
woodburj <89941683+woodburj@users.noreply.github.com>
2021-09-06 15:47:46 +0100

    Created script.js to gnerate phot assignement

---
+++ script.js
@@ -0,0 +1,14 @@
+const locations = ['Glasgow', 'Edinburgh', 'New York', 'Paris', 'London','Munich', 'Cape Cod'];
+const subjects = ['a Seascape', 'the Milk Way', 'a bride', 'a street scene', 'a building', 'a landscpe', 'a pet animal', 'a wild animal', 'a person at work', 'a sports person'];
+const cameraGears = ['pin-hole camera', 'DSLR + 50mm lens', 'DSLR + 14mm lens', 'FF Mirrorless + 70-200mm lens', 'camera phone'];
+
+let randomLocation = Math.floor(Math.random() * (locations.length -1));
+let randomSubject = Math.floor(Math.random() * (subjects.length -1));
+let randomGear = Math.floor(Math.random() * (cameraGears.length -1));
+
+let location = locations[randomLocation];
+let subject = subjects[randomSubject];
+let cameraGear = cameraGears[randomGear];
+
+console.log (`Your next assignement is to go to ${location}, take a photograph of ${subject} using only a ${cameraGear}`)
+console.log('L:' + randomLocation + ' S:' + randomSubject + ' G:' + randomGear);
